697966fb75519f1c11464ba2eb143d1a0a13a67c
Parth Sharma <bhawanal1978@gmail.com>
2024-12-18 18:57:11 +0530

    Completed Backend API's

---
+++ backend/lib/redis.js
@@ -0,0 +1,8 @@
+import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config({});
+//INFO: It is a key value store.
+
+export const redis = new Redis(process.env.REDIS_URL);
+await redis.set("key", "value");
